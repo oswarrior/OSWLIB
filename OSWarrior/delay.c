5f7ab932d3fbd7351f6771f634d4609874192965
argandas <hugo.arganda@gmail.com>
2014-05-01 00:23:55 -0500

    funcion micros()
    Se arregle bug en la funcion micros()

--- OSWarrior/delay.c
+++ OSWarrior/delay.c
@@ -1,4 +1,3 @@
-
 #include "oswlib/settings.h"
 #include "oswlib/OSWarrior/delay.h"		//Delay control
 
@@ -20,49 +19,50 @@ void ( * _OSWarrior_tickISR_handler )(void) = _OSWarrior_Dummy_Fnc;
 
 /*
 ** ===================================================================
-**     Function : oswarrior_enableDelays
-**
-**     Description :
-**         This function is a dummy, only for avoid errors on Rx handler
-**     
-**     Parameters  : Nothing
-**     Returns     : Nothing
+** Delay functions definition
 ** ===================================================================
 */
 
 void oswarrior_enableDelays(void)
 {
-	OSWARRIOR_ENABLE_DELAYS;
+	#ifdef __OSWarrior_DK__
+	{
+		TPM3SC = 0x00u; 							/* Disable TPM3 Module */
+		TPM3MOD = timerCountForMillisecond();		/* Set timer period to 1 millisecond */ 
+		(void)TPM3SC; 								/* Clear any pending flags */
+		
+		#if DELAY_PS == 1
+			TPM3SC = 0x08;		/* PS = 1 */			
+		#elif DELAY_PS == 2
+			TPM3SC = 0x09;		/* PS = 2 */				
+		#elif DELAY_PS == 4
+			TPM3SC = 0x0A;		/* PS = 4 */ 		
+		#elif DELAY_PS == 8
+			TPM3SC = 0x0B;		/* PS = 8 */ 		
+		#elif DELAY_PS == 16
+			TPM3SC = 0x0C;		/* PS = 16 */ 		
+		#elif DELAY_PS == 32
+			TPM3SC = 0x0D;		/* PS = 32 */ 		
+		#elif DELAY_PS == 64
+			TPM3SC = 0x0E;		/* PS = 64 */		
+		#else
+			TPM3SC = 0x00;		/* Disable TPM3 Module */
+			#error "The selected Prescaler is not supported"
+		#endif
+  
+		TPM3SC |= 0x40;				/* Enable Timer Overflow ISR */ 		
+	}
+	#endif
 };
-
-/*
-** ===================================================================
-**     Function : oswarrior_disableDelays
-**
-**     Description :
-**         This function is a dummy, only for avoid errors on Rx handler
-**     
-**     Parameters  : Nothing
-**     Returns     : Nothing
-** ===================================================================
-*/
 
 void oswarrior_disableDelays(void)
 {
-	OSWARRIOR_DISABLE_DELAYS
+	#ifdef __OSWarrior_DK__
+	{
+		TPM3SC = 0x00u;		/* Disable TPM3 Module */
+	}
+	#endif	
 };
-
-/*
-** ===================================================================
-**     Function : delay
-**
-**     Description :
-**         This function is a dummy, only for avoid errors on Rx handler
-**     
-**     Parameters  : Nothing
-**     Returns     : Nothing
-** ===================================================================
-*/
 
 void delayMicroseconds(unsigned int us)
 {
@@ -76,22 +76,13 @@ void delay(unsigned int ms)
 	while( ( millis() - start_time ) < ms );
 };
 
-/*
-** ===================================================================
-**     Function : millis
-**
-**     Description :
-**         This function is a dummy, only for avoid errors on Rx handler
-**     
-**     Parameters  : Nothing
-**     Returns     : Nothing
-** ===================================================================
-*/
-
 T_ULONG micros(void)
 {
-	T_ULONG temp = TPM3MOD * 1000 / TPM3CNT;
-	return (_OSWarrior_msCounter * 1000) + temp;
+	#ifdef __OSWarrior_DK__
+	{
+		return (TPM3CNT * 1000 / timerCountForMillisecond()) + (_OSWarrior_msCounter * 1000);
+	}
+	#endif		
 };
 
 T_ULONG millis(void)
@@ -99,20 +90,20 @@ T_ULONG millis(void)
 	return _OSWarrior_msCounter;
 };
 
-/*
-** ===================================================================
-**     Interrupt : fnc_oswarrior_tick_counter
-**
-**     Description :
-**          This interruption is programmed to run every time a 
-**          millisecond happens. 
-**     
-** ===================================================================
-*/
-
-__interrupt void fnc_oswarrior_tick_counter(void)
-{    
+__interrupt void oswarrior_tick_counter(void)
+{
 	_OSWarrior_msCounter++;
-	OSWARRIOR_TICK_ACK;
+	oswarrior_tick_ack();
 	(*_OSWarrior_tickISR_handler)();
 };
+
+void oswarrior_tick_ack(void)
+{
+	#ifdef __OSWarrior_DK__
+	{
+		(void)TPM3SC;
+		TPM3SC_TOF = 0;		/* Clear Timer Overflow Flag */
+	}
+	#endif		
+}
+
--- OSWarrior/delay.h
+++ OSWarrior/delay.h
@@ -8,24 +8,26 @@
 #ifndef OSWARRIOR_DELAYS_H_
 	#define OSWARRIOR_DELAYS_H_
 
+	#define timerCountForMillisecond() ( ( BUSCLOCK / 1000u ) / DELAY_PS )
+
 	/*
 	** ===================================================================
 	** Delay interrupt handlers
 	** ===================================================================
 	*/
 
-	extern __interrupt void fnc_oswarrior_tick_counter(void);
+	extern __interrupt void oswarrior_tick_counter(void);
 
-	static void (* near const _vect_TickISR[])(void) @OSWarrior_ISR_TPM3Ovf_Vector = {
-			fnc_oswarrior_tick_counter
-	};
+	#ifdef __OSWarrior_DK__
+		static void (* near const _vect_TickISR[])(void) @OSWarrior_ISR_TPM3Ovf_Vector = { oswarrior_tick_counter };
+	#endif
 	    
 	/*
     ** ===================================================================
     ** Delay functions prototypes
     ** ===================================================================
     */
-	
+		
 	PUBLIC_FCT void oswarrior_enableDelays(void);
 	PUBLIC_FCT void oswarrior_disableDelays(void);
 	
@@ -34,6 +36,8 @@
 	
 	PUBLIC_FCT T_ULONG millis(void);
 	PUBLIC_FCT T_ULONG micros(void);
+
+	PRIVATE_FCT void oswarrior_tick_ack(void);
 
 	PUBLIC_DATA void ( * _OSWarrior_tickISR_handler )(void);
 

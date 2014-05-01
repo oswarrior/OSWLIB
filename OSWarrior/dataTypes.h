6962525059c62d9c9f7ae43c1f0e9007a98ef6b0
argandas <hugo.arganda@gmail.com>
2014-05-01 00:23:01 -0500

    Datos para ISR
    Se crea un tipo de datos para cuando se trabaja con polling o ISR.

--- OSWarrior/dataTypes.h
+++ OSWarrior/dataTypes.h
@@ -1,34 +1,3 @@
-/* =============================================================================
-** 					OSWarrior - EMBEDDED SYSTEMS SOFTWARE            	  	  
-** =============================================================================
-**                       	OBJECT SPECIFICATION                                
-** =============================================================================
-**  Filename: 	 dataTypes.h
-**  Description: OSWarrior data type definitions
-** =============================================================================
-**  Author:		 Hugo Arganda (hugo.arganda@gmail.com)
-**  Version:	 4.0
-**  Status: 	 Integrated
-** =============================================================================
-**  							CHANGE LOG:
-** =============================================================================
-**	Date: 27/03/2013 - dataTypes.h Version 2.0 freeze
-** =============================================================================
-**  							 LICENSE:
-** =============================================================================
-**  This library is free software; you can redistribute it and/or
-**  modify it under the terms of the Attribution-ShareAlike
-**  License as published by the Creative Commons Organization; either
-**  version 3.0 of the License, or (at your option) any later version.
-**  This library is distributed in the hope that it will be useful,
-**  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-**  See the Attribution-ShareAlike License for more details.
-**  You should have received a copy of the Creative Commons Attribution-ShareAlike 
-**  License along with this library; if not send a email to the library author.
-** =============================================================================
-*/
-
 #ifndef OSWARRIOR_DATATYPES_H_
 	
 	#define OSWARRIOR_DATATYPES_H_
@@ -241,6 +210,15 @@
 		RECEIVE = 0u,
 		TRANSMIT
 	}E_DATA_DIR;
+
+	//-----------------------------------------------------------------------------------------------
+	
+
+	typedef enum
+	{
+		POL = 0u,
+		ISR
+	}E_FNC_METHOD;
 
 	//-----------------------------------------------------------------------------------------------
 
